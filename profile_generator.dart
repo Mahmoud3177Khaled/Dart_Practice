c50ab3ca9d225fdc5e5f02d6b91bcdf2e1516506
Mahmoud Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2025-04-21 03:38:54 +0200

    minor comments improvment

--- profile_generator.dart
+++ profile_generator.dart
@@ -1,28 +1,35 @@
 import 'dart:io';
 
 void main() {
-  // college: once he states his college, he can't change it -> a final string to be unchangeable once set
+  // college: once user states his college, he can't change it... therefor... a final string will be unchangeable once set
+  // and also late, cause im 100% sure it will be assigned before being used
   late final String college;
 
   // year will be a constant 2025... thats just today
   const int year = 2025;
 
-  // pretty much no one changes thier name... so i made it final to be 
+  // pretty much no one changes thier name... so i made it final to be immutable once set
+  // and also late, cause im 100% sure it will be assigned before being used
   late final String name;
 
   // IDs can take many forms, it might be at a time only numbers or another numbers with chars...
-  // so we use dynamic to handle that, and we use late to make sure it gets assigned
+  // so we use dynamic to handle that...
+  // and also late, cause im 100% sure it will be assigned before being used
   late dynamic id;
 
-  // we let var figure out the major type, a string, a char, a num...  even if null, we just store "undeclared" 
+  // we let var figure out the major type, a string, a char, a num... who knows...
+  // even if it were null, we just store "undeclared"
+  // and late cause it 100% wont get used before being assigned too
   late var major;
 
-  // an intermidietry input variable, to chech user input before assigning it to a variable
+  // an intermidietry input variable, to check user input before assigning it to a variable
+  // needs to be dynamic, as not all inputs are the same type 
   dynamic input;
 
   // for the college, name, and id... since a student must have all those entered (can't just store a student
-  // without a name, a college or an id)...  if he skips input, i keep asking him for them, avoiding crashes, 
-  // and ensuring they are always assigned a value, once so, the loops breaks and we procced to the next field
+  // without a name, a college or an id)...  so if user skips input, i keep asking him for them, avoiding crashes, 
+  // and ensuring they are always assigned a value before being used,
+  // once so, the loops breaks and we procced to the next field
   while(true) {
 
     print("\nCollege: ");
